d602d4368e33aa5c913116ad54c9d32ebb053bd7
Dylan Dang <dylan@tokenize.exchange>
2023-09-08 10:22:43 +0700

    feat: hide footer content

--- src/layouts/components/NavFooter.vue
+++ src/layouts/components/NavFooter.vue
@@ -3,7 +3,7 @@
   <footer
     class="flex items-center h-12 mt-5 text-sm bg-gray-100 dark:bg-[#171d30] py-2 z-10 w-full"
   >
-    <div class="flex flex-1">
+    <!-- <div class="flex flex-1">
       &copy;&nbsp;
       {{ new Date().getFullYear() }}&nbsp;
       Made With&nbsp;
@@ -32,6 +32,6 @@
         target="noopener noreferrer"
         >Github</a
       >
-    </div>
+    </div> -->
   </footer>
 </template>
