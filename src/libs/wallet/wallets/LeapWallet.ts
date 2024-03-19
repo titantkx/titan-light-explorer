df5055882ca1b53c5d3a278e95bdc1cccaa15f77
binhth <binh@tokenize.exchange>
2024-03-19 10:13:47 +0700

    feat(ttc-155): implement add titan chain to wallet

---
+++ src/libs/wallet/wallets/LeapWallet.ts
@@ -0,0 +1,180 @@
+import type { Transaction } from '@/libs/utils/type';
+import { makeSignDoc as makeSignDocAmino } from '@cosmjs/amino';
+import { createWasmAminoConverters } from '@cosmjs/cosmwasm-stargate';
+import { fromBase64, fromBech32, toHex } from '@cosmjs/encoding';
+import {
+  makeAuthInfoBytes,
+  makeSignDoc,
+  Registry,
+  type OfflineSigner,
+  type TxBodyEncodeObject,
+} from '@cosmjs/proto-signing';
+import { AminoTypes, createAuthzAminoConverters } from '@cosmjs/stargate';
+import { PubKey } from 'cosmjs-types/cosmos/crypto/secp256k1/keys';
+import { SignMode } from 'cosmjs-types/cosmos/tx/signing/v1beta1/signing';
+import { TxRaw } from 'cosmjs-types/cosmos/tx/v1beta1/tx';
+import { Any } from 'cosmjs-types/google/protobuf/any';
+import {
+  keyType,
+  WalletName,
+  type AbstractWallet,
+  type Account,
+  type WalletArgument,
+} from '../Wallet';
+
+export class LeapWallet implements AbstractWallet {
+  name: WalletName.Leap;
+  chainId: string;
+  registry: Registry;
+  conf: WalletArgument;
+  signer: OfflineSigner | any;
+  aminoTypes = new AminoTypes({
+    ...createAuthzAminoConverters(),
+    ...createWasmAminoConverters(),
+  });
+  constructor(arg: WalletArgument, registry: Registry, sign?: OfflineSigner) {
+    this.name = WalletName.Leap;
+    this.signer = sign;
+    this.chainId = arg.chainId || 'cosmoshub';
+    // @ts-ignore
+    if (!window.getOfflineSigner || !window.leap) {
+      throw new Error('Please install Leap extension');
+    }
+    this.registry = registry;
+    this.conf = arg;
+  }
+  async getAccounts(): Promise<Account[]> {
+    // const chainId = 'cosmoshub'
+    // @ts-ignore
+    await window.leap.enable(this.chainId);
+
+    // @ts-ignore
+    this.signer = await window.leap.getOfflineSigner(this.chainId);
+    return (await this.signer.getAccounts()) as unknown as Account[];
+  }
+  supportCoinType(coinType?: string | undefined): Promise<boolean> {
+    return Promise.resolve(true);
+  }
+  isEthermint() {
+    return this.conf.hdPath && this.conf.hdPath.startsWith("m/44'/60");
+  }
+  async sign(transaction: Transaction): Promise<TxRaw> {
+    return this.signAmino(transaction);
+  }
+  // @deprecated use signAmino instead
+  // because signDirect is not supported ledger wallet
+  async signDirect(transaction: Transaction): Promise<TxRaw> {
+    const accouts = await this.getAccounts();
+    const hex = toHex(fromBech32(transaction.signerAddress).data);
+    const accountFromSigner = accouts.find(
+      (account) => toHex(fromBech32(account.address).data) === hex
+    );
+    if (!accountFromSigner) {
+      throw new Error('Failed to retrieve account from signer');
+    }
+    const pubkey = Any.fromPartial({
+      typeUrl: keyType(transaction.chainId),
+      value: PubKey.encode({
+        key: accountFromSigner.pubkey,
+      }).finish(),
+    });
+    const txBodyEncodeObject: TxBodyEncodeObject = {
+      typeUrl: '/cosmos.tx.v1beta1.TxBody',
+      value: {
+        messages: transaction.messages,
+        memo: transaction.memo,
+      },
+    };
+    const txBodyBytes = this.registry.encode(txBodyEncodeObject);
+    const gasLimit = Number(transaction.fee.gas);
+    const authInfoBytes = makeAuthInfoBytes(
+      [{ pubkey, sequence: transaction.signerData.sequence }],
+      transaction.fee.amount,
+      gasLimit,
+      transaction.fee.granter,
+      transaction.fee.payer
+    );
+    const signDoc = makeSignDoc(
+      txBodyBytes,
+      authInfoBytes,
+      transaction.chainId,
+      transaction.signerData.accountNumber
+    );
+
+    // @ts-ignore
+    // const offlineSigner = await window.leap.getOfflineSignerAuto(this.chainId);
+    // console.log(offlineSigner)
+    const { signature, signed } = await this.signer.signDirect(
+      transaction.signerAddress,
+      signDoc
+    );
+    return TxRaw.fromPartial({
+      bodyBytes: signed.bodyBytes,
+      authInfoBytes: signed.authInfoBytes,
+      signatures: [fromBase64(signature.signature)],
+    });
+  }
+
+  async signAmino(tx: Transaction): Promise<TxRaw> {
+    const accouts = await this.getAccounts();
+    const hex = toHex(fromBech32(tx.signerAddress).data);
+    const accountFromSigner = accouts.find(
+      (account) => toHex(fromBech32(account.address).data) === hex
+    );
+    if (!accountFromSigner) {
+      throw new Error('Failed to retrieve account from signer');
+    }
+    // const pubkey = encodePubkey(encodeSecp256k1Pubkey(accountFromSigner.pubkey));
+    const pubkey = Any.fromPartial({
+      typeUrl: keyType(tx.chainId),
+      value: PubKey.encode({
+        key: accountFromSigner.pubkey,
+      }).finish(),
+    });
+    const signMode = SignMode.SIGN_MODE_LEGACY_AMINO_JSON;
+    const msgs = tx.messages.map((msg) => this.aminoTypes.toAmino(msg));
+    const signDoc = makeSignDocAmino(
+      msgs,
+      tx.fee,
+      tx.chainId,
+      tx.memo,
+      tx.signerData.accountNumber,
+      tx.signerData.sequence
+    );
+
+    // @ts-ignore
+    // const offlineSigner = window.getOfflineSigner(this.chainId)
+    const { signature, signed } = await this.signer.signAmino(
+      tx.signerAddress,
+      signDoc
+    );
+
+    // console.log(signature, 'signature', signed);
+
+    const signedTxBody = {
+      messages: signed.msgs.map((msg: any) => this.aminoTypes.fromAmino(msg)),
+      memo: signed.memo,
+    };
+    const signedTxBodyEncodeObject: TxBodyEncodeObject = {
+      typeUrl: '/cosmos.tx.v1beta1.TxBody',
+      value: signedTxBody,
+    };
+    const signedTxBodyBytes = this.registry.encode(signedTxBodyEncodeObject);
+
+    const signedGasLimit = Number(signed.fee.gas);
+    const signedSequence = Number(signed.sequence);
+    const signedAuthInfoBytes = makeAuthInfoBytes(
+      [{ pubkey, sequence: signedSequence }],
+      signed.fee.amount,
+      signedGasLimit,
+      signed.fee.granter,
+      signed.fee.payer,
+      signMode
+    );
+    return TxRaw.fromPartial({
+      bodyBytes: signedTxBodyBytes,
+      authInfoBytes: signedAuthInfoBytes,
+      signatures: [fromBase64(signature.signature)],
+    });
+  }
+}
---
+++ src/libs/wallet/wallets/MetamaskWallet.ts
@@ -0,0 +1,183 @@
+import type { Transaction } from '@/libs/utils/type';
+import { fromBase64, fromHex, toBase64 } from '@cosmjs/encoding';
+import { makeAuthInfoBytes, Registry } from '@cosmjs/proto-signing';
+import { AminoTypes, createDefaultAminoConverters } from '@cosmjs/stargate';
+import { hashMessage } from '@ethersproject/hash';
+import { computePublicKey, recoverPublicKey } from '@ethersproject/signing-key';
+import {
+  createEIP712,
+  generateFee,
+  generateMessageWithMultipleTransactions,
+  generateTypes,
+} from '@tharsis/eip712';
+import type { Chain } from '@tharsis/transactions';
+
+import { PubKey } from 'cosmjs-types/cosmos/crypto/secp256k1/keys';
+import { TxRaw } from 'cosmjs-types/cosmos/tx/v1beta1/tx';
+import { Any } from 'cosmjs-types/google/protobuf/any';
+import { ethermintToEth, ethToEthermint } from '../../utils/format';
+import { defaultMessageAdapter } from '../EthermintMessageAdapter';
+import {
+  extractChainId,
+  keyType,
+  WalletName,
+  type AbstractWallet,
+  type Account,
+  type WalletArgument,
+} from '../Wallet';
+
+export class MetamaskWallet implements AbstractWallet {
+  name: WalletName.Metamask;
+  chainId: string;
+  registry: Registry;
+  prefix: string;
+  aminoTypes = new AminoTypes(createDefaultAminoConverters());
+
+  constructor(arg: WalletArgument, registry: Registry) {
+    this.name = WalletName.Metamask;
+    this.chainId = arg.chainId || 'cosmoshub';
+    this.registry = registry;
+    this.prefix = arg.prefix || 'evmos';
+  }
+
+  async getAccounts(): Promise<Account[]> {
+    // @ts-ignore
+    if (!window.ethereum || !window.ethereum.request) {
+      throw new Error('Please install Metamask extension');
+    }
+
+    // @ts-ignore46
+    const accounts = await window.ethereum.request({
+      method: 'eth_requestAccounts',
+    });
+
+    const message = 'Verify Public Key';
+    // @ts-ignore
+    const signature = await window?.ethereum?.request({
+      method: 'personal_sign',
+      params: [message, accounts[0], ''],
+    });
+
+    const uncompressedPk = recoverPublicKey(hashMessage(message), signature);
+    const hexPk = computePublicKey(uncompressedPk, true);
+    const pk = toBase64(fromHex(hexPk.replace('0x', '')));
+
+    const connected = accounts.map((address: any) => ({
+      address: ethToEthermint(address, this.prefix),
+      algo: 'secp256k1',
+      pubkey: pk,
+    }));
+
+    localStorage.setItem('metamask-connected', JSON.stringify(connected));
+
+    return connected;
+  }
+
+  async getConnectedAccounts() {
+    const connected = localStorage.getItem('metamask-connected');
+    if (connected) {
+      return JSON.parse(connected) as Account[];
+    }
+    return this.getAccounts();
+  }
+
+  async supportCoinType(coinType?: string): Promise<boolean> {
+    // Here, you can check if Metamask supports a specific type of cryptocurrency.
+    return true;
+  }
+
+  async sign(transaction: Transaction): Promise<TxRaw | Uint8Array> {
+    const { signerAddress, messages, fee, signerData } = transaction;
+
+    const senderHexAddress = ethermintToEth(signerAddress);
+    const chain: Chain = {
+      chainId: extractChainId(signerData.chainId),
+      cosmosChainId: signerData.chainId,
+    };
+
+    // getAccounts
+    const accounts = await this.getConnectedAccounts();
+    const account = accounts.find(
+      (acc) => ethermintToEth(acc.address) === senderHexAddress
+    );
+    if (!account) {
+      throw new Error('Account not found');
+    }
+
+    const pubkeyBytes = String(account.pubkey);
+
+    const sender = {
+      accountAddress: transaction.signerAddress,
+      sequence: transaction.signerData.sequence,
+      accountNumber: transaction.signerData.accountNumber,
+      pubkey: pubkeyBytes,
+    };
+
+    const fees = generateFee(
+      transaction.fee.amount[0].amount,
+      transaction.fee.amount[0].denom,
+      transaction.fee.gas,
+      transaction.signerAddress
+    );
+
+    const msgs = transaction.messages.map((x) => this.aminoTypes.toAmino(x));
+    const toSignTx = generateMessageWithMultipleTransactions(
+      sender.accountNumber.toString(),
+      sender.sequence.toString(),
+      transaction.signerData.chainId,
+      transaction.memo,
+      fees,
+      msgs
+    );
+
+    const types = generateTypes(
+      defaultMessageAdapter[transaction.messages[0].typeUrl].getTypes()
+    );
+    const eip712Payload = createEIP712(types, chain.chainId, toSignTx);
+
+    // Obtaining the data required for MetaMask signature
+    // @ts-ignore
+    // const acc = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    // console.log("accounts: ", acc)
+
+    // const eip712Payload = JSON.stringify(messages[0]);
+
+    // Signing an EIP-712 payload using MetaMask.
+    // @ts-ignore
+    const signature = await window.ethereum.request({
+      method: 'eth_signTypedData_v4',
+      params: [senderHexAddress, JSON.stringify(eip712Payload)],
+    });
+
+    // Creating a TxRaw object after signing.
+    const signatureBytes = fromHex(signature.replace('0x', ''));
+    const txBodyBytes = this.registry.encode({
+      typeUrl: '/cosmos.tx.v1beta1.TxBody',
+      value: { messages },
+    });
+
+    // NOTE: assume ethsecp256k1 by default because after mainnet is the only one that is going to be supported
+    const pubkey = Any.fromPartial({
+      typeUrl: keyType(signerData.chainId),
+      value: PubKey.encode({
+        key: fromBase64(pubkeyBytes),
+      }).finish(),
+    });
+    const authInfoBytes = makeAuthInfoBytes(
+      [{ pubkey, sequence: signerData.sequence }],
+      fee.amount,
+      Number(fee.gas),
+      undefined, // feeGranter
+      undefined // feePayer
+    );
+
+    //  Using TxRaw.fromPartial to create a TxRaw object with attribute values.
+    const signedTx: TxRaw = TxRaw.fromPartial({
+      bodyBytes: txBodyBytes,
+      authInfoBytes: authInfoBytes,
+      signatures: [signatureBytes],
+    });
+
+    return signedTx; // Create the txRaw
+  }
+}
