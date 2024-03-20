265fbb20a2f2bdc9b9b440b2f7e10968a0b2b627
phuongtq <tranphuong.080@gmail.com>
2024-03-20 14:47:27 +0700

    feat(router): update auto redirect from root path

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,21 @@
-import { useBlockchain } from "@/stores";
-import { createRouter, createWebHistory } from "vue-router";
+import { useBlockchain } from '@/stores';
+import { createRouter, createWebHistory } from 'vue-router';
 // @ts-ignore
-import { setupLayouts } from "virtual:generated-layouts";
+import { setupLayouts } from 'virtual:generated-layouts';
 // @ts-ignore
-import routes from "~pages";
+import routes from '~pages';
+
+var networkType = 'mainnet';
+if (window.location.hostname.search('testnet') > -1) {
+  networkType = 'testnet';
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      redirect: '/Titan%20Testnet',
+      redirect: networkType == 'testnet' ? '/Titan%20Testnet' : '/Titan',
     },
     ...setupLayouts(routes),
   ],
@@ -18,14 +23,14 @@ const router = createRouter({
 
 //update current blockchain
 router.beforeEach((to) => {
-    const { chain } = to.params
-    if(chain){
-      const blockchain = useBlockchain()
-      if(chain !== blockchain.chainName) {
-        blockchain.setCurrent(chain.toString())
-      }
-    } 
-})
+  const { chain } = to.params;
+  if (chain) {
+    const blockchain = useBlockchain();
+    if (chain !== blockchain.chainName) {
+      blockchain.setCurrent(chain.toString());
+    }
+  }
+});
 
 // Docs: https://router.vuejs.org/guide/advanced/navigation-guards.html#global-before-guards
 
