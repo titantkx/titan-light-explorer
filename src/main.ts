3955d018c01cff760c6730a1e9ec3742d58e99bf
binhth <binh@tokenize.exchange>
2024-04-25 15:19:46 +0700

    fix(ttc-207): show contract sates in cosmwasm transaction

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,14 @@ import '@/style.css';
 import LazyLoad from 'lazy-load-vue3';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
+import { VueDd } from 'vue-dd';
 
 import router from './router';
 
 // Create vue app
 const app = createApp(App);
+// register component
+app.component('VueDd', VueDd);
 // Use plugins
 app.use(i18n);
 app.use(createPinia());
